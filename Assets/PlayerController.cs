b60042e790166735a4cb5883b1784bb92a0b3586
Rick Heemskerk <37400964+bidoofgoo@users.noreply.github.com>
2019-05-27 10:37:03 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        control();
+    }
+
+    void control()
+    {
+        float hori = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float verti = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector3 pos = this.transform.position;
+        Vector3 newPos = new Vector3(pos.x + hori, pos.y + verti, pos.z);
+        this.transform.position = newPos;
+    }
+}
