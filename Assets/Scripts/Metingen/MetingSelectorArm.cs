048fb0289459b96b44673e89932336c3328f4499
Rick Heemskerk <37400964+bidoofgoo@users.noreply.github.com>
2019-06-05 15:09:54 +0200

    Arm uses metingUpdater
    de arm beweeggg

---
+++ Assets/Scripts/Metingen/MetingSelectorArm.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetingSelectorArm : ADropdownUpdate
+{
+    private Vector3[] positions = { new Vector3(-2.7f, 0, 0), new Vector3(-1.6f, -1.75f, 0)};
+    private Vector3[] rotations = { new Vector3(0, 180, 0), new Vector3(-180, -75, 90) };
+
+    public override void doTheUpdate(int meting)
+    {
+        if(positions[meting] != null || rotations[meting] != null)
+        {
+            this.transform.position = positions[meting];
+            this.transform.rotation = Quaternion.Euler(rotations[meting]);
+        }
+    }
+}
