4adfbae9a6250c576f39949a3febb5a5e0c6aff2
Roy Admiraal <s1081752@student.hsleiden.nl>
2019-06-05 09:42:27 +0200

    Dropdownhandler
    Dropdown handler

---
+++ Assets/changeDropdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeDropdown : MonoBehaviour
+{
+    public Camera cam;
+    public Dropdown myDropdown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(myDropdown.value)
+        {
+            case 0:
+                cam.backgroundColor = Color.white;
+                break;
+
+            case 1:
+                cam.backgroundColor = Color.red;
+                break;
+
+            case 2:
+                cam.backgroundColor = Color.blue;
+                break;
+
+            case 3:
+                cam.backgroundColor = Color.green;
+                break;
+
+        }
+    }
+}
